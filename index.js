db106d124bb2d60d64ea5b0ca8b676f2d90ac226
eddysoniwu <iwuohaeddyson2400@gmail.com>
2024-06-13 11:04:13 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const roll = setTimeout(function() {
+    document.getElementById('roll').style.display = 'block';
+}, 2000)
+const reel = setTimeout(function() {
+    document.getElementById('reel').style.display = 'block';
+}, 4000)
+const tri = setTimeout(function() {
+    document.getElementById('load').style.display = 'block';
+}, 6000)
+const ri = setTimeout(function() {
+    document.getElementById('load').style.fontSize = 'large';
+    document.getElementById('load').style.color = 'green';
+}, 8000)
+const ti = setTimeout(function() {
+    document.getElementById('load').style.fontSize = 'x-large';
+    document.getElementById('load').style.color = 'blue';
+}, 10000)
+const trit = setTimeout(function() {
+    document.getElementById('load').style.fontSize = 'x-large';
+    document.getElementById('load').style.color = 'red';
+}, 12000)
+const treat = setTimeout(function() {
+    document.getElementById('load').style.fontSize = 'x-large';
+    document.getElementById('load').style.color = 'orange';
+}, 14000)
+const trea = setTimeout(function() {
+    document.getElementById('load').style.fontSize = 'x-large';
+    document.getElementById('load').style.color = 'yellowgreen';
+}, 16000)
+const tr = setTimeout(function() {
+    document.getElementById('load').style.fontSize = 'x-large';
+    document.getElementById('load').style.color = 'orangered';
+}, 18000);
+const tret = setTimeout(function() {
+    document.getElementById('load').style.fontSize = 'larger';
+    document.getElementById('load').style.color = '#000';
+}, 20000);
+const tryu = setTimeout(function() {
+    document.getElementById('load').style.fontSize = 'x-large';
+    document.getElementById('load').style.color = 'white';
+}, 22000);
+const ed = setTimeout(function() {
+    window.location = "home.html";
+}, 27000);
